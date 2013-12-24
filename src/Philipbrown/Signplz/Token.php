943b56ff9dc35350fed847d5004b5a4cfa4f08b7
Philip Brown <phil@ipbrown.com>
2013-12-24 15:06:16 +0000

    Add Token

---
+++ src/Philipbrown/Signplz/Token.php
@@ -0,0 +1,47 @@
+<?php namespace Philipbrown\Signplz;
+
+class Token {
+
+  /**
+   * @var string
+   */
+  protected $key;
+
+  /**
+   * @var secret
+   */
+  protected $secret;
+
+  /**
+   * Construct
+   *
+   * @param string $key
+   * @param string $secret
+   */
+  public function __construct($key, $secret)
+  {
+    $this->key = $key;
+    $this->secret = $secret;
+  }
+
+  /**
+   * Get Key
+   *
+   * @return string
+   */
+  public function getKey()
+  {
+    return $this->key;
+  }
+
+  /**
+   * Get Secret
+   *
+   * @return string
+   */
+  public function getSecret()
+  {
+    return $this->secret;
+  }
+
+}
