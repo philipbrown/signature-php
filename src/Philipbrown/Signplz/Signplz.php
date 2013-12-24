39dbc8697ae883ce32955fb16ba08a88d4ae5072
Philip Brown <phil@ipbrown.com>
2013-12-24 15:05:54 +0000

    Add Signplz

--- src/Philipbrown/Signplz/Signplz.php
+++ src/Philipbrown/Signplz/Signplz.php
@@ -1,3 +1,29 @@
 <?php namespace Philipbrown\Signplz;
 
-class Signplz {}
+class Signplz {
+
+  /**
+   * Token
+   *
+   * @param string $key
+   * @param string $secret
+   * @return Philipbrown\Signplz\Token
+   */
+  public function token($key, $secret)
+  {
+    return new Token($key, $secret);
+  }
+
+  /**
+   * Request
+   *
+   * @param string $method
+   * @param string $path
+   * @param array $params
+   */
+  public function request($method, $path, array $params)
+  {
+    return new Request($method, $path, $params);
+  }
+
+}
